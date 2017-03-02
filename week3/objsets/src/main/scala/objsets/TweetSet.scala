9b2b2149575bce72dcaf1d526654a8093a69117f
Jorge Rodriguez <jorgerod84@gmail.com>
2017-03-02 21:12:17 +0100

    commit week2

--- week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, this)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -107,8 +107,8 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
   /**
    * The following methods are already implemented
    */
@@ -124,8 +124,8 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = filterAcc(p, acc.incl(elem))
+
     
   /**
    * The following methods are already implemented
